c1ff2b4f12a925f4a460375950ce3d514faf0512
xiaowang <2081896995@qq.com>
2019-06-14 08:30:47 +0800

    图片验证码

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
 # Create your views here.
 
 class RegisterView(View):
@@ -8,3 +9,14 @@ class RegisterView(View):
 
         return render(request, 'users/register.html')
 
+
+
+class ImageCodeView(View):
+
+    def get(self, request, image_code_id):
+        # 接入图片验证码接口
+        text, image_bytes = None
+        connect = get_redis_connect('veriftions')
+        img_key = f'img_{image_code_id}'
+        connect.setex(img_key, 300, text)
+        return HttpResponse(content=image_bytes, content_type='image/jpg')
